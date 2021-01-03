33a634c19ea8070efbb0aca2f35dc594314c7147
Davide Lanfranchi <davidelanfranchi@users.noreply.github.com>
2021-01-03 16:43:15 +0100

    Improve CSS animations

--- src/scripts/pages/cart/CartMessage.js
+++ src/scripts/pages/cart/CartMessage.js
@@ -5,8 +5,10 @@ import "./CartMessage.scss";
 function CartSubmitted(props) {
   return (
     <div className="CartMessage">
-      <h2 className="CartMessage__heading">{props.heading}</h2>
-      <p className="CartMessage__message">{props.message}</p>
+      <div className="CartMessage__panel">
+        <h2 className="CartMessage__heading">{props.heading}</h2>
+        <p className="CartMessage__message">{props.message}</p>
+      </div>
     </div>
   );
 }
