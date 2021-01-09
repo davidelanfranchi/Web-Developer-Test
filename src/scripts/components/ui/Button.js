8c45872d77c25bb764dca60fd28e95c01c7a3552
Davide Lanfranchi <davidelanfranchi@users.noreply.github.com>
2021-01-09 22:02:13 +0100

    Add some CSS hover effects

--- src/scripts/components/ui/Button.js
+++ src/scripts/components/ui/Button.js
@@ -9,7 +9,7 @@ function Button(props) {
       disabled={props.isDisabled}
       onClick={props.onClickHandler}
     >
-      Buy Now
+      <span className="Button__label">Buy Now</span>
       {props.hasSpinner && <span className="Button__spinner"></span>}
     </button>
   );
